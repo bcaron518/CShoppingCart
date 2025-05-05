b9a8781ed95538d52cdb18961c05dcccf8477c83
Benjamin Caron <caron518@gmail.com>
2025-05-05 10:54:08 -0400

    Create shop.c

---
+++ shop.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include "shop.h"
+
+void shop_initialize() {
+    // Populate shop and item details
+}
+
+void shop() {
+    // Implementation of shop selection
+}
+
+void items() {
+    // Implementation of item selection
+}
+
+void cart() {
+    // Implementation of cart functionality
+}
