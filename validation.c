9509d24b5e16ef7956f093dfeee1160a0b579321
Benjamin Caron <caron518@gmail.com>
2025-05-05 10:54:50 -0400

    Create validation.c

---
+++ validation.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include "validation.h"
+
+int validate() {
+    // Implementation of user input validation
+}
+
+void account_validate() {
+    // Implementation of account validation
+}
